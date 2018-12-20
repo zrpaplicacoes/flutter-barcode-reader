099463f35b07ed79e2a5c5b76a00a5e1fc39e3d1
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2018-12-20 19:04:22 -0200

    Added required hardware acceleration statement on AndroidManifest

--- android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
@@ -1,6 +1,5 @@
 package com.apptreesoftware.barcodescan
 
-import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
@@ -8,40 +7,31 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.KeyEvent
 import android.view.MenuItem
-import android.view.View
 import android.view.WindowManager
-import android.widget.Button
 
 import com.journeyapps.barcodescanner.CaptureManager
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.journeyapps.barcodescanner.ViewfinderView
 import com.yourcompany.barcodescan.R
 
-class BarcodeScannerActivity : AppCompatActivity(), DecoratedBarcodeView.TorchListener {
+class BarcodeScannerActivity : AppCompatActivity() {
 
     private var capture: CaptureManager? = null
     private var barcodeScannerView: DecoratedBarcodeView? = null
-    private var switchFlashlightButton: Button? = null
     private var viewfinderView: ViewfinderView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barcode_scanner)
 
-        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner)
-        barcodeScannerView!!.setTorchListener(this)
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner) as DecoratedBarcodeView
 
-        switchFlashlightButton = findViewById<Button>(R.id.switch_flashlight)
+        barcodeScannerView?.setBackgroundColor(Color.argb(100, 0, 0, 0))
 
-        viewfinderView = findViewById(R.id.zxing_viewfinder_view)
+        viewfinderView = findViewById(R.id.zxing_viewfinder_view) as ViewfinderView
 
-        // if the device does not have flashlight in its camera,
-        // then remove the switch flashlight button...
-        if (!hasFlash()) {
-            switchFlashlightButton!!.visibility = View.GONE
-        }
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
 
         setSupportActionBar(toolbar)
 
@@ -54,8 +44,6 @@ class BarcodeScannerActivity : AppCompatActivity(), DecoratedBarcodeView.TorchLi
         capture = CaptureManager(this, barcodeScannerView)
         capture!!.initializeFromIntent(intent, savedInstanceState)
         capture!!.decode()
-
-        changeMaskColor(null)
     }
 
     override fun onResume() {
@@ -82,41 +70,11 @@ class BarcodeScannerActivity : AppCompatActivity(), DecoratedBarcodeView.TorchLi
         return barcodeScannerView!!.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event)
     }
 
-    override fun onTorchOn() {
-        switchFlashlightButton!!.setText(R.string.turn_off_flashlight)
-    }
-
-    override fun onTorchOff() {
-        switchFlashlightButton!!.setText(R.string.turn_on_flashlight)
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
             finish()
             return true
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    fun switchFlashlight(@Suppress("UNUSED_PARAMETER") view: View) {
-        if (getString(R.string.turn_on_flashlight) == switchFlashlightButton!!.text) {
-            barcodeScannerView!!.setTorchOn()
-        } else {
-            barcodeScannerView!!.setTorchOff()
-        }
-    }
-
-    /**
-     * Check if the device's camera has a Flashlight.
-     * @return true if there is Flashlight, otherwise false.
-     */
-    private fun hasFlash(): Boolean {
-        return applicationContext.packageManager
-                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
-    }
-
-    private fun changeMaskColor(@Suppress("UNUSED_PARAMETER") view: View?) {
-        val color = Color.argb(100, 0, 0, 0)
-        viewfinderView!!.setBackgroundColor(color)
     }
 }
